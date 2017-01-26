5f10d7cda565c87877214c0d6280d55e4693d3c0
Scott O'Malley <scottyomalley@gmail.com>
2017-01-26 19:12:54 +0000

    In class examples

--- Week1/app/js/sample-controller.js
+++ Week1/app/js/sample-controller.js
@@ -1,4 +1,44 @@
 angular.module('app')
-.controller('SampleController', ['$scope', function($scope) {
-  $scope.title = 'The title in the first controller';
-}]);
+  .controller('SampleController', ['$scope', function ($scope) {
+    $scope.title = 'The title in the first controller';
+
+    $scope.books = [
+      {
+        name: 'Name of the wind',
+        cost: 200
+      },
+      {
+        name: 'Face of the wind',
+        cost: 300
+      },
+      {
+        name: 'Arm of the wind',
+        cost: 100
+      },
+
+    ];
+
+    $scope.cart = [];
+
+    $scope.addToCart = function (book) {
+      $scope.cart.push(book);
+    }
+
+    $scope.total = function() {
+
+     var totalValue = $scope.cart.reduce(function (previous, current) {
+        return previous + current.cost;
+      }, 0);
+
+      return totalValue;
+    }
+
+    $scope.$watch('cart', function(){
+      $scope.total = $scope.cart.reduce(function (previous, current) {
+        return previous + current.cost;
+      }, 0);
+    });
+
+
+
+  }]);
