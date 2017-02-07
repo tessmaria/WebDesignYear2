2699274a951e02a100734fdd624e3dde1cc7dc95
Teresa Amass <teresa_amass@hotmail.com>
2017-02-07 00:45:31 +0000

    working on week3 exercise 1

--- Week3/app/js/week-three-controller.js
+++ Week3/app/js/week-three-controller.js
@@ -1,4 +1,8 @@
-angular.module('app')
-.controller('WeekThreeController', ['$scope', function($scope) {
-  $scope.weekThreeTitle = 'Week3';
+app.controller('ParentController', ['$scope', function($scope) {
+	$scope.title = 'Parent Controller';
+	$scope.description = 'This is inside of the parent controller';
+}]);
+
+app.controller('ChildCtrl', ['$scope', function($scope) {
+	$scope.title = 'Child Controller Title';
 }]);
