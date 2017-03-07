5f2a1137bf168bdaf238fa8fe42272f523c1bd33
Teresa Amass <teresa_amass@hotmail.com>
2017-03-07 00:27:04 +0000

    got pixabay to work

--- Week3/app/js/image-controller.js
+++ Week3/app/js/image-controller.js
@@ -1,5 +1,5 @@
 angular.module('app')
-.controller('ImageController', ["$scope", "http", function($scope, $http) {
+.controller('ImageController', ["$scope", "$http", function($scope, $http) {
 	$scope.title = "Pixabay Images";
 	$http.get('https://pixabay.com/api/?key=4745151-bcf7b0141175f6a3a8bf365b1&q=blue+flowers&image_type=photo')
     .then(function(pictures){
--- Week3/app/js/phone-controller.js
+++ Week3/app/js/phone-controller.js
@@ -1,8 +1,7 @@
 angular.module('app')
 .controller('PhoneController', ['$scope', '$http', function($scope, $http) {
 	$scope.title = "Here is a list of phones";
-	$scope.phones = undefined;
-  	$scope.pictures = undefined;
+
 
   $http.get('../phones.json').then(function(phones){
     $scope.phones = phones.data;
